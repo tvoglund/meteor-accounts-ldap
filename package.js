41fac6092026e329037750076ed19066e1cd8e63
tvoglund <tvoglund@gmail.com>
2015-10-12 23:00:42 -0600

    cleaned up code.  found out that jdap.js is the file that atmosphere makes me do a publish on each server.  It must have binary dependencies.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name : 'tvoglund:meteor-accounts-ldap',
   summary: 'Meteor account login via LDAP',
-  version: '0.0.8',
+  version: '0.0.14',
   git : 'https://github.com/tvoglund/meteor-accounts-ldap',
   documentation: 'README.md'
 });
@@ -9,12 +9,10 @@ Package.describe({
 Npm.depends({'ldapjs' : '0.7.1', 'connect' : '2.19.3'});
 
 Package.on_use(function (api) {
-  api.versionsFrom('1.1.0.2');
+  api.versionsFrom('METEOR@1.1.0.2');
   api.use(['routepolicy', 'webapp'], 'server');
   api.use(['accounts-base', 'underscore'], ['client', 'server']);
-  api.use('accounts-password', 'server');
   api.imply('accounts-base', ['client', 'server']);
-  api.use(['ui', 'templating', 'jquery', 'spacebars', 'reactive-var'], 'client');
   api.add_files(['meteor-accounts-ldap_client.js'], 'client');
   api.add_files('meteor-accounts-ldap_server.js', 'server');
   api.export('LDAP');
