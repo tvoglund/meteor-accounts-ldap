2108795c094496d03c7e2f563dc41b23be5aea3c
tvoglund <tvoglund@gmail.com>
2015-10-27 19:37:54 -0600

    fixed bug with auto create user flag

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name : 'tvoglund:meteor-accounts-ldap',
   summary: 'Meteor account login via LDAP',
-  version: '0.0.18',
+  version: '0.0.19',
   git : 'https://github.com/tvoglund/meteor-accounts-ldap',
   documentation: 'README.md'
 });
@@ -12,6 +12,7 @@ Package.on_use(function (api) {
   api.versionsFrom('METEOR@1.1.0.2');
   api.use(['routepolicy', 'webapp'], 'server');
   api.use(['accounts-base', 'underscore'], ['client', 'server']);
+  api.use('accounts-password', 'server');
   api.imply('accounts-base', ['client', 'server']);
   api.add_files(['meteor-accounts-ldap_client.js'], 'client');
   api.add_files('meteor-accounts-ldap_server.js', 'server');
